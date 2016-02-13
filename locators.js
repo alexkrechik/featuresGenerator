4388155f0ea209ca6d614af8e8de9abc0fb46454
Alex Krechik <alexander.krechick@lifestreet.com>
2016-02-13 20:15:34 +0200

    2-levels locators using

--- locators.js
+++ locators.js
@@ -1,13 +1,15 @@
 var locators = {
-	inputSearch : '//input[@name="q"]',
-	btnSearch : '//input[@name="btnK"]'
+	mainPage: {
+		inputSearch : '//input[@name="q"]',
+		btnSearch : '//input[@name="btnK"]'
+	}
 };
 
 var markFoundElement = function (element) {
 	element.style.setProperty('border','thin solid #0000FF','important');
 };
 
-var markLocator = function (locator) {
+var markLocator = function (page, locator) {
 	try {
 		var element = document.evaluate(locator, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 		if (element) {
@@ -18,8 +20,12 @@ var markLocator = function (locator) {
 	}
 };
 
-for (var locator in locators) {
-	if (locators.hasOwnProperty(locator)) {
-		markLocator(locators[locator]);
+for (var page in locators) {
+	if (locators.hasOwnProperty(page)) {
+		for (var locator in locators[page]) {
+			if (locators[page].hasOwnProperty(locator)) {
+				markLocator(page,locators[page][locator]);
+			}
+		}
 	}
 }
