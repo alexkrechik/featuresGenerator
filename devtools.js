07fad5e8657b1f1f8a7acd199b596342bdc045fb
Alex Krechik <scraych@gmail.com>
2016-06-09 19:12:58 +0300

    Attached menu.js to the panel

--- devtools.js
+++ devtools.js
@@ -1,30 +1,36 @@
 // Create a new panel
 chrome.devtools.panels.create("Features Generator",
-	null,
-	"panel.html",
-	function(extensionPanel) {
-		var _window;
+    null,
+    "panel.html",
+    function(extensionPanel) {
+        var _window;
 
-		//If panel is opened - show message, add it to data[] otherwise
-		var data = [];
-		var port = chrome.runtime.connect({name: 'devtools'});
-		port.onMessage.addListener(function (msg) {
-			if(_window) {
-				_window.addStepText(msg.step);
-			} else {
-				data.push(msg.step);
-			}
-		});
+        //If panel is opened - show message, add it to data[] otherwise
+        var data = [];
+        var port = chrome.runtime.connect({name: 'devtools'});
+        port.onMessage.addListener(function (msg) {
+            if(_window) {
+                _window.addStepText(msg.step);
+            } else {
+                data.push(msg.step);
+            }
+        });
 
-		//Add all the messages got before panel was opened
-		extensionPanel.onShown.addListener(function tmp(panelWindow) {
-			extensionPanel.onShown.removeListener(tmp);
-			_window = panelWindow;
-			var msg;
-			while (msg = data.shift())
-				_window.addStepText(msg.step);
+        //Add all the messages got before panel was opened
+        extensionPanel.onShown.addListener(function tmp(panelWindow) {
+            extensionPanel.onShown.removeListener(tmp);
+            _window = panelWindow;
+            var msg;
+            while (msg = data.shift())
+                _window.addStepText(msg.step);
+	        
+	        chrome.devtools.inspectedWindow.getResources(function (resources) {
+		        var menu = resources.find(function(el){ return el.url.match(/menu.js/)});
+		        menu.getContent(function(content) {
+			        _window.menu = content;
+		        });
+	        });
+        });
 
-		});
-
-	}
+    }
 );
