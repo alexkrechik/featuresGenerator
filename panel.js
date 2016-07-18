d4624a3c43c6ebd55c298cebad62a978e0a67ddb
Alex Krechik <scraych@gmail.com>
2016-07-18 18:45:32 +0300

    Methods refactoring

--- panel.js
+++ panel.js
@@ -52,150 +52,19 @@ var steps = [
 	]}
 ];
 
-function generateStepText(text) {
-	if (document.getElementById("bdd-term-replace").checked) {
-		var currStep = text.match(/^[A-Za-z]*/)[0];
-		var stepsArr = document.getElementById("generatedFeatures").innerText.split(/\n/);
-		var lastStep = "";
-		for (var i = stepsArr.length - 1; i >=0; i--) {
-			lastStep = stepsArr[i].match(/^[A-Za-z]*/)[0];
-			if (lastStep !== 'And') {
-				if (lastStep === currStep) {
-					var re = new RegExp("^" + currStep);
-					text = text.replace(re, "And");
-				}
-				return text;
-			}
-		}
-	} else {
-		return text;
-	}
-}
+//******** COMMON METHODS ********************************************************************************************//
 
-function autoComplete() {
-	return document.getElementById("auto-steps-complete").checked;
-}
-
-function addText (text) {
-	document.getElementById("generatedFeatures").innerHTML += "<div>" + text + "</div>";
-}
-
-function addStepText(text) {
-	addText(generateStepText(text));
-}
-
-function markHighlightStatus(step) {
-	if (autoComplete()) {
-		inspectedWindowEval('currSuggestion = \'' + step + '\';', function(result, err) {
-			if (err) {
-				addText(JSON.stringify(err) + err.message);
-			}
-		});
-	}
-}
-
-function getlocators(data) {
-	//Remove everything before module.exports
-	data = data.replace(/.*module.exports.*/,'');
-	//Remove last close and everething after it
-	data = data.replace(/};?(\r\n|\n|\r)*$/,'');
-	var evalStr = 'var func = function(){' + data + '}';
-	eval(evalStr);
-	function require() {
-		return new Proxy({},{
-			get: function(target) {
-				return new Proxy(target, {
-					get: function() {return new Proxy(target,{
-						apply: function() {return ''}
-					})},
-					apply: function() {return ''}
-				});
-			}
-		});
-	}
-	return func();
-}
-
-function clearLocator(fileName, locatorsPageName) {
-	var element = document.getElementById('uploaded-locator-id' + fileName);
-	document.getElementById('uploaded-locators').removeChild(element);
-	chrome.devtools.inspectedWindow.eval('delete locators["' + locatorsPageName + '"];',{useContentScriptContext: true}, function(result, err) {
-		if (err) {
-			addText(JSON.stringify(err) + err.message);
-		}
-	});
-}
-
-function addUploadedLocatorsFile(locatorsPageName, fileName) {
-	try {
-		var element = document.createElement('div');
-		element.setAttribute('id','uploaded-locator-id' + fileName);
-		element.innerText =  "'" + fileName + "' file was uploaded to '" + locatorsPageName + "' locators page ";
-		var removeElement = document.createElement('button');
-		removeElement.innerText = "Remove uploaded locator";
-		removeElement.addEventListener('click', function() {
-			clearLocator(fileName, locatorsPageName);
-		});
-		element.appendChild(removeElement);
-		document.getElementById('uploaded-locators').appendChild(element);
-	} catch(err) {
-		addText(err + " " + err.message);
-	}
+function addError(err, text) {
+	var msg = "";
+	text && (msg += text + ":::");
+	err && (msg += JSON.stringify(err));
+	err.message && (msg += ":::" + JSON.stringify(err.message));
+	alert(msg);
 }
 
 function inspectedWindowEval(command, callback) {
 	chrome.devtools.inspectedWindow.eval(command, {useContentScriptContext: true}, callback);
 }
-
-document.getElementsByClassName('file-input')[0].addEventListener('change', function(){
-	var file = this.files[0];
-	var reader = new FileReader();
-	reader.onload = function(){
-		try {
-			var text = reader.result;
-			locatorsString = getlocators(text);
-		} catch(err) {
-			addText(err + " " + err.message);
-		}
-	};
-	reader.readAsText(file);
-},false);
-
-document.getElementById('upload-locators').addEventListener('click', function(){
-	var strJSON = JSON.stringify(locatorsString);
-	var replaces = document.getElementById('page-object-replace').value;
-	var arr = JSON.parse(replaces);
-	arr.forEach(function(strArr){
-		var from = strArr[0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-		var to = strArr[1];
-		var regex = new RegExp(from, "g");
-		strJSON = strJSON.replace(regex,to);
-	});
-	var locatorsPageName = document.getElementById('page-object-name').value;
-	var strAddLocatorsCommand = 'locators["' + locatorsPageName + '"] =' + strJSON + ';';
-	inspectedWindowEval(strAddLocatorsCommand, function(result, err){
-		if (err) {
-			addText(JSON.stringify(err) + err.message);
-		}
-		var fileName = document.getElementsByClassName('file-input')[0].files[0].name;
-		try {
-			addUploadedLocatorsFile(locatorsPageName, fileName);
-			inspectedWindowEval('processLocators();');
-			document.getElementById('page-object-name').value = "";
-			document.getElementsByClassName('file-input')[0].value="";
-		} catch(err) {
-			addText(err + " " + err.message);
-		}
-	});
-},false);
-
-document.getElementById('auto-steps-complete').addEventListener('click', function(e){
-	if(autoComplete()) {
-		document.getElementById('steps_suggestions_block').style.display = 'table-cell';
-	} else {
-		document.getElementById('steps_suggestions_block').style.display = 'none';
-	}
-});
 
 function getStepsArr(steps) {
 	var arr = [];
@@ -225,27 +94,165 @@ function filterSteps(steps, text) {
 	return res;
 }
 
-function getDivFocusText() {
-	var text = window.getSelection().focusNode.parentElement.innerText;
-	var textArr = text.split('\n');
-	return textArr[textArr.length - 1];
-}
-
-function setDivFocusText(text, newline) {
-	window.getSelection().focusNode.textContent = text;
-}
-
-function getCurrText(e) {
-	var text = getDivFocusText();
-	var code = e.keyCode;
-	var letter = String.fromCharCode(code);
-	if (letter.match(/[A-Z]/)) {
-		text += letter.toLocaleLowerCase();
-	} else if(e.keyCode === 8) {
-		text = text.substr(0, text.length - 1);
+function markHighlightStatus(step) {
+	if (autoComplete()) {
+		inspectedWindowEval('currSuggestion = \'' + step + '\';', function(result, err) {
+			if (err) {
+				addError(err, 'Error during statuses highlighting');
+			}
+		});
 	}
-	return text;
 }
+
+function removeCurrentClass(e) {
+	e.className = e.className.replace( new RegExp('(?:^|\\s)current_suggestion(?!\\S)') ,'');
+}
+
+function addCurrentClassName(e) {
+	e.className += ' current_suggestion';
+}
+
+function eventKeyDownDocument(event) {
+	if(autoComplete()) {
+		switch(event.keyCode) {
+			case 38:
+				highlightSuggestion('previous');
+				break;
+			case 40:
+				highlightSuggestion('next');
+				break;
+			case 13:
+				var currEl = document.getElementsByClassName('current_suggestion')[0];
+				if (currEl) {
+					insertStep(currEl.text);
+					break;
+				}
+		}
+	}
+}
+
+//******** HEAD SIDE OF EXTENSION ************************************************************************************//
+
+function autoComplete() {
+	return document.getElementById("auto-steps-complete").checked;
+}
+
+function getlocators(data) {
+	//Remove everything before module.exports
+	data = data.replace(/.*module.exports.*/,'');
+	//Remove last close and everething after it
+	data = data.replace(/};?(\r\n|\n|\r)*$/,'');
+	var evalStr = 'var func = function(){' + data + '}';
+	eval(evalStr);
+	function require() {
+		return new Proxy({},{
+			get: function(target) {
+				return new Proxy(target, {
+					get: function() {return new Proxy(target,{
+						apply: function() {return ''}
+					})},
+					apply: function() {return ''}
+				});
+			}
+		});
+	}
+	return func();
+}
+
+function clearLocator(fileName, locatorsPageName) {
+	var element = document.getElementById('uploaded-locator-id' + fileName);
+	document.getElementById('uploaded-locators').removeChild(element);
+	chrome.devtools.inspectedWindow.eval('delete locators["' + locatorsPageName + '"];',{useContentScriptContext: true}, function(result, err) {
+		if (err) {
+			addError(err, 'Error during locators clearing');
+		}
+	});
+}
+
+function addUploadedLocatorsFile(locatorsPageName, fileName) {
+	try {
+		var element = document.createElement('div');
+		element.setAttribute('id','uploaded-locator-id' + fileName);
+		element.innerText =  "'" + fileName + "' file was uploaded to '" + locatorsPageName + "' locators page ";
+		var removeElement = document.createElement('button');
+		removeElement.innerText = "Remove uploaded locator";
+		removeElement.addEventListener('click', function() {
+			clearLocator(fileName, locatorsPageName);
+		});
+		element.appendChild(removeElement);
+		document.getElementById('uploaded-locators').appendChild(element);
+	} catch(err) {
+		addError(err, 'Error during locator file element creation');
+	}
+}
+
+function eventFileInput(){
+	var file = this.files[0];
+	var reader = new FileReader();
+	reader.onload = function(){
+		try {
+			var text = reader.result;
+			locatorsString = getlocators(text);
+		} catch(err) {
+			addText(err, 'Error during file reading');
+		}
+	};
+	reader.readAsText(file);
+}
+
+function applyReplaces(str) {
+	var replaces = document.getElementById('page-object-replace').value;
+	if (replaces) {
+		var arr = JSON.parse(replaces);
+		arr.forEach(function(strArr){
+			var from = strArr[0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+			var to = strArr[1];
+			var regex = new RegExp(from, "g");
+			str = str.replace(regex,to);
+		});
+	}
+	return str;
+}
+
+function eventUploadLocators(){
+	try {
+		var strJSON = JSON.stringify(locatorsString);
+		strJSON = applyReplaces(strJSON);
+		var locatorsPageName = document.getElementById('page-object-name').value;
+		if (!locatorsPageName) {
+			throw 'Page name should not be empty';
+		}
+	} catch(err) {
+		addError(err, 'Upload locators error');
+	}
+	var strAddLocatorsCommand = 'locators["' + locatorsPageName + '"] =' + strJSON + ';';
+	addText(strAddLocatorsCommand);
+	inspectedWindowEval(strAddLocatorsCommand, function(result, err){
+		if (err) {
+			addError(err, 'Applying locators error');
+		} else {
+			var fileName = document.getElementsByClassName('file-input')[0].files[0].name;
+			try {
+				addUploadedLocatorsFile(locatorsPageName, fileName);
+				inspectedWindowEval('processLocators();');
+				document.getElementById('page-object-name').value = "";
+				document.getElementsByClassName('file-input')[0].value="";
+			} catch(err) {
+				addError(err, 'Locators processing error');
+			}
+		}
+	});
+}
+
+function eventAutoStepsComplete(e){
+	if(autoComplete()) {
+		document.getElementById('steps_suggestions_block').style.display = 'table-cell';
+	} else {
+		document.getElementById('steps_suggestions_block').style.display = 'none';
+	}
+}
+
+//******** STEPS VARIANTS ********************************************************************************************//
 
 function createSuggestion(text) {
 	var element = document.createElement('a');
@@ -255,32 +262,10 @@ function createSuggestion(text) {
 	element.style.display = 'flex';
 	element.style.margin = "5";
 	element.addEventListener('click', function() {
-		window.getSelection().focusNode.textContent = text;
+		setDivFocusText(text);
 	});
 	return element;
 }
-
-function populateAutoComplete(e) {
-	if(autoComplete()) {
-		document.getElementById('steps_suggestions').innerHTML = "";
-		var resSteps;
-		var currText = getCurrText(e);
-		var stepsArr = getStepsArr(steps);
-		resSteps = filterSteps(stepsArr, currText);
-		resSteps.forEach(function(step){
-			document.getElementById('steps_suggestions').appendChild(createSuggestion(step));
-		});
-		highlightSuggestion();
-	}
-}
-
-document.getElementById('generatedFeatures').addEventListener('keydown', function (e) {
-	var arrCode = [38, 40, 13];
-	var keyCode = e.keyCode;
-	if (arrCode.indexOf(keyCode) === -1) {
-		populateAutoComplete(e);
-	}
-});
 
 function highlightSuggestion(num) {
 	var suggestions = document.getElementsByClassName('suggestion');
@@ -312,53 +297,117 @@ function highlightSuggestion(num) {
 	}
 }
 
-function removeCurrentClass(e) {
-	e.className = e.className.replace( new RegExp('(?:^|\\s)current_suggestion(?!\\S)') ,'');
+//******** GENERATED STEPS *******************************************************************************************//
+
+function addText (text) {
+	document.getElementById("generatedFeatures").innerHTML += "<div>" + text + "</div>";
 }
 
-function addCurrentClassName(e) {
-	e.className += ' current_suggestion';
+function generateStepText(text) {
+	if (document.getElementById("bdd-term-replace").checked) {
+		var currStep = text.match(/^[A-Za-z]*/)[0];
+		var stepsArr = document.getElementById("generatedFeatures").innerText.split(/\n/);
+		var lastStep = "";
+		for (var i = stepsArr.length - 1; i >=0; i--) {
+			lastStep = stepsArr[i].match(/^[A-Za-z]*/)[0];
+			if (lastStep !== 'And') {
+				if (lastStep === currStep) {
+					var re = new RegExp("^" + currStep);
+					text = text.replace(re, "And");
+				}
+				return text;
+			}
+		}
+	} else {
+		return text;
+	}
+}
+
+function addStepText(text) {
+	addText(generateStepText(text));
+}
+
+function getDivFocusText() {
+	var text = window.getSelection().focusNode.parentElement.innerText;
+	var textArr = text.split('\n');
+	return textArr[textArr.length - 1];
+}
+
+function getCurrText(e) {
+	//TODO - get everething works with getDivFocusText only
+	var text = getDivFocusText();
+	var code = e.keyCode;
+	var letter = String.fromCharCode(code);
+	if (letter.match(/[A-Z]/)) {
+		text += letter.toLocaleLowerCase();
+	} else if(e.keyCode === 8) {
+		text = text.substr(0, text.length - 1);
+	}
+	return text;
+}
+
+function setDivFocusText(text, newline) {
+
+	//Insert currStep text
+	var sel, range;
+	sel = window.getSelection();
+	window.getSelection().focusNode.textContent = "";
+	range = sel.getRangeAt(0);
+	var newNode = document.createTextNode(text);
+	range.insertNode(newNode);
+
+	//move the cursor
+	range.setStartAfter(newNode);
+	range.setEndAfter(newNode);
+	sel.removeAllRanges();
+	sel.addRange(range);
+
+	//newline
+	if (newline) {
+		var div = document.createElement("div");
+		sel.focusNode.parentElement.parentElement.appendChild(div);
+	}
 }
 
 function insertStep(currStep) {
 	var textContent = getDivFocusText();
 	if (currStep && currStep !== textContent) {
-		//Insert currStep text
-		var sel, range;
-		sel = window.getSelection();
-		window.getSelection().focusNode.textContent = "";
-		range = sel.getRangeAt(0);
-		var newNode = document.createTextNode(currStep);
-		range.insertNode(newNode);
-
-		//move the cursor
-		range.setStartAfter(newNode);
-		range.setEndAfter(newNode);
-		sel.removeAllRanges();
-		sel.addRange(range);
-
-		//prevent default actions
-		event.preventDefault();
-		window.getSelection().focusNode.textContent = currStep;
+		setDivFocusText(currStep);
 		markHighlightStatus(currStep);
+		event.preventDefault();
 	}
 }
 
-document.addEventListener('keydown', function(event) {
+function populateAutoComplete(e) {
 	if(autoComplete()) {
-		switch(event.keyCode) {
-			case 38:
-				highlightSuggestion('previous');
-				break;
-			case 40:
-				highlightSuggestion('next');
-				break;
-			case 13:
-				var currEl = document.getElementsByClassName('current_suggestion')[0];
-				if (currEl) {
-					insertStep(currEl.text);
-					break;
-				}
-		}
+		document.getElementById('steps_suggestions').innerHTML = "";
+		var resSteps;
+		var currText = getCurrText(e);
+		var stepsArr = getStepsArr(steps);
+		resSteps = filterSteps(stepsArr, currText);
+		resSteps.forEach(function(step){
+			document.getElementById('steps_suggestions').appendChild(createSuggestion(step));
+		});
+		highlightSuggestion();
 	}
-});
+}
+
+function generatedFeaturesKeyDown(e) {
+	var arrCode = [38, 40, 13];
+	var keyCode = e.keyCode;
+	if (arrCode.indexOf(keyCode) === -1) {
+		populateAutoComplete(e);
+	}
+}
+
+//******** EVENT LISTENERS *******************************************************************************************//
+
+document.getElementsByClassName('file-input')[0].addEventListener('change', eventFileInput);
+
+document.getElementById('upload-locators').addEventListener('click', eventUploadLocators);
+
+document.getElementById('auto-steps-complete').addEventListener('click', eventAutoStepsComplete);
+
+document.getElementById('generatedFeatures').addEventListener('keydown', generatedFeaturesKeyDown);
+
+document.addEventListener('keydown', eventKeyDownDocument);
